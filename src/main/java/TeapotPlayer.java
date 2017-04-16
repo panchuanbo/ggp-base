83a95a856c0fdec45f2e58a7f7674a71e371e99c
Chuanbo Pan <chuanbo@stanford.edu>
2017-04-16 00:00:38 -0700

    player completes assign2 parts 1,2

---
+++ src/main/java/TeapotPlayer.java
@@ -0,0 +1,68 @@
+import java.util.List;
+import java.util.Random;
+
+import org.ggp.base.apps.player.Player;
+import org.ggp.base.player.gamer.exception.GamePreviewException;
+import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
+import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.cache.CachedStateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
+
+public class TeapotPlayer extends StateMachineGamer {
+
+	Player p;
+
+	@Override
+	public StateMachine getInitialStateMachine() {
+		// TODO Auto-generated method stub
+		return new CachedStateMachine(new ProverStateMachine());
+	}
+
+	@Override
+	public void stateMachineMetaGame(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Move stateMachineSelectMove(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		// TODO Auto-generated method stub
+
+		StateMachine machine = getStateMachine();
+		List<Move> moves = machine.getLegalMoves(getCurrentState(), getRole());
+
+		return moves.get((new Random()).nextInt(moves.size()));
+	}
+
+	@Override
+	public void stateMachineStop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stateMachineAbort() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void preview(Game g, long timeout) throws GamePreviewException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "418 I'm a Teapot";
+	}
+
+}
